fe437b7ce0b453dbaca4ba44cfabc58e4f32417b
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2019-10-22 12:39:57 +0200

    update

--- ModuleApp/api/src/main/java/module-info.java
+++ ModuleApp/api/src/main/java/module-info.java
@@ -2,6 +2,7 @@ import moduleapp.api.Hello;
 import moduleapp.api.apiimpl.HelloImpl;
 
 module moduleapp.api {
+    requires transitive java.logging;
     exports moduleapp.api;
       provides Hello with HelloImpl;
 }
